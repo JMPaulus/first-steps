aa4ab317788c0c80e035c17c8468a7869d87f62c
Jedrzej Paulus <codeboy@codeboy.pl>
2018-12-03 22:04:53 +0100

    Take a look at this loop. Please
    Bart Simpson helped me to write a message in this loop.

---
+++ Look at that loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+
+using namespace std;
+int main (int argc, char** argv) {
+
+	for(int i=1; i<5; i++)
+	
+	{	
+	cout<< i << " Bart Simpson wrote on the board: Talk to me, I am sure we will find many topics to discuss our future cooperation."<< endl;
+	}
+	return 0;
+	
+	
+}
